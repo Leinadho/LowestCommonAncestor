e70f88239a2eaac90222262bb8f9d0d34af9f10f
Daniel <Daniel@GLaDOS>
2017-10-19 18:22:55 +0100

    Created node class. Next: testing. Maybe I should put it in a different branch?

---
+++ src/Node.java
@@ -0,0 +1,46 @@
+
+/**
+ * 
+ * @author Daniel Cosgrove
+ *
+ *	This is a class to represent the nodes in a binary tree, 
+ *	Each node points to it's unique parent, and each node can have only two children
+ *	The tree should only be built using methods of the nodes, which prevent 
+ *	a non-binary tree from being constructed
+ *	(specifically: we will get a binary, acyclic Directed graph)
+ *
+ */
+public class Node {
+
+	int nChildren;
+	Node parent;
+	
+	public Node(){
+		nChildren = 0;
+		parent = null;
+	}
+
+	
+	public boolean addParent(Node parent){
+		if(parent.nChildren < 2){
+			this.parent = parent;
+			parent.nChildren++;
+			return true;
+		}
+		else{
+			return false;
+		}
+	}
+	
+	public boolean addChild(Node child){
+		if(nChildren < 2){
+			child.parent = this;
+			this.nChildren++;
+			return true;
+		}
+		else{
+			return false;
+		}
+	}
+	
+}
