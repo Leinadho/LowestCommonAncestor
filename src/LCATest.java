294541bf052db846b57c1538ef8726610e660008
Daniel <Daniel@GLaDOS>
2017-10-31 14:42:37 +0000

    Wrote some tests for LCA in DAG.
    I'm tempted to use the definition in this paper:
    https://www.sciencedirect.com/science/article/pii/S0020019010000487
    The lowest SINGLE common ancestor seems to make more sense (given the
    previous task) than a group of lowest ancestors. However given that the
    requirement is ambiguous, perhaps I should implement both.

--- src/LCATest.java
+++ src/LCATest.java
@@ -105,5 +105,66 @@ public class LCATest {
 		assertEquals("Reflexive case", tool.LCA(v, v), v);
 	}
 	
+	/**
+	 * PART II: DAG TESTS
+	 */
+	
+	//TODO: enumerate cases you wish to test, write tests for them 
+	
+	@Test
+	public void testPyramidalDAG(){
+		//Create Data
+		Node a = new Node(),b = new Node(),c = new Node();
+		b.addParent(c);					//	 /--------->		
+		b.addParent(a);					//	b--> c --> a	
+		c.addParent(a);
+		
+		//Tests
+		assertTrue("", tool.LCA(b,c) == c);	 //based on the self-descendant property
+		assertFalse("", tool.LCA(b,c) == a); //c is a lower 'level' from the root than a, therefore a cannot be the lowest common ancestor
+		assertTrue("", tool.LCA(b,a) == a);
+		assertFalse("", tool.LCA(b,a) == c); // c is not an ancestor of a
+		assertFalse("", tool.LCA(b,a) == b); // b is not an ancestor of a
+	}
+	
+	@Test
+	public void testMaximallyConnectedPyramidalDAG(){
+		//Create Data
+		Node a = new Node(),b = new Node(),c = new Node(), d = new Node();
+		a.addChild(c);					//	 		
+		a.addChild(b);					//		a		
+		b.addParent(c);					//    >   <
+		c.addChild(d);					//	 /     \
+		d.addParent(b);					//	b---->	c
+										//	 <-- 	 <
+										//	    \---- d 
+		//Tests
+		
+	}
+	
+	
+	@Test
+	public void testLargeDAG(){
+		//Create data
+		Node[] nodes = new Node[5];
+		for(int i = 0; i < nodes.length; i++){
+			nodes[i] = new Node();
+		}
+		nodes[0].addChild(nodes[1]);	//		0 
+		nodes[0].addChild(nodes[2]);	// 	1-		-2
+		nodes[1].addChild(nodes[3]);	//   \ / \  /   
+		nodes[1].addChild(nodes[4]);	//    3   4       
+		nodes[2].addChild(nodes[3]);	
+		nodes[2].addChild(nodes[4]);
+		
+		//Tests
+		assertEquals(tool.LCA(nodes[1], nodes[2] ), nodes[0]);
+		assertEquals(tool.LCA(nodes[1], nodes[3] ), nodes[0]);
+		assertEquals(tool.LCA(nodes[3], nodes[4] ), nodes[0]);
+		assertFalse(tool.LCA(nodes[3], nodes[4] ) == nodes[1]);
+		assertFalse(tool.LCA(nodes[3], nodes[4] ) == nodes[2]);
+		assertFalse(tool.LCA(nodes[3], nodes[4] ) == nodes[3]);
+		
+	}
 	
 }
